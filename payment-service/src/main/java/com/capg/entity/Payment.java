d4de9e6ccbcd0388c90e93f493ec40d831ae20c9
Sourabh <india@DESKTOP-4PAIJTK>
2022-11-18 13:51:58 +0530

    Payment-service

---
+++ payment-service/src/main/java/com/capg/entity/Payment.java
@@ -0,0 +1,23 @@
+package com.capg.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity @Data @AllArgsConstructor @NoArgsConstructor
+public class Payment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int paymentId;
+	private String paymentStatus;
+	private String transactionId;
+	private int orderId;
+	private int amount;
+	
+
+}
